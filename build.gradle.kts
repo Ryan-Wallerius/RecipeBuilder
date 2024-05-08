97abca3ae8a40b3b54e556000febdebe4b4e1fc0
Ryan Wallerius <ryan.wallerius@avanade.com>
2024-05-08 11:11:11 -0700

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.2.5"
+	id("io.spring.dependency-management") version "1.1.4"
+}
+
+group = "ryanwallerius"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_22
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	runtimeOnly("com.microsoft.sqlserver:mssql-jdbc")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/test/java/ryanwallerius/recipebuilder/RecipeBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package ryanwallerius.recipebuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
